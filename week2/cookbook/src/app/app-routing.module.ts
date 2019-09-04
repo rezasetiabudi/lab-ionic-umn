4045aba15f649cc1eb1f640d65e8a7efd97e8324
rezasb <shinichisetiabudi@gmail.com>
2019-09-04 13:02:13 +0700

    Week4 (#2)
    * add function to delete recipe and reload with component reload
    
    * commit week 04

--- week2/cookbook/src/app/app-routing.module.ts
+++ week2/cookbook/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
         loadChildren: './recipes/recipes.module#RecipesPageModule'
       },
       {
-        path: 'recipe-detail',
+        path: ':recipeId',
         loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
       }
     ]
